b8eb7b6fceec4edd483dca7477ac3806aff4af43
viviurbano <viviane.urbano1@gmail.com>
2022-08-15 13:57:21 -0400

    update firebase.js
    settting up env APIKYES

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: '${process.env.REACT_APP_API_KEY}',
+  authDomain: '${process.env.REACT_APP_AUTH_DOMAIN}',
+  projectId: 'wddm-120-capstone',
+  storageBucket: 'wddm-120-capstone.appspot.com',
+  messagingSenderId: '1082082141260',
+  appId: '1:1082082141260:web:7c1cf9651991ad4ae9c0e9',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
